f69910db66b85866c901adb4f917bd17d5b27419
karinaaltheaabad <karinaaltheaabad@users.noreply.github.com>
2021-04-08 23:10:26 -0700

    restructured files

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const data = require('./data.json');
+const app = express(); 
+
+//setting pug as a template engine for your app
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
+app.get('/home', (req, res) => {
+    res.render('index.html');
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.html');
+})
+
+app.listen(3000);
